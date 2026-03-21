bcb63f6b445d92bd889b33569437842ff16abfe8
Ryutaro Kawaguchi <ryu@ryumacbookair2026.local>
2026-03-22 02:23:03 +0900

    chore: disable autoUpdate on brew command

--- nix/nix-darwin/homebrew.nix
+++ nix/nix-darwin/homebrew.nix
@@ -14,6 +14,13 @@
     enable = true;
     user = "ryu";
 
+    global.autoUpdate = false;
+    onActivation = {
+      autoUpdate = false;
+      upgrade = true;
+      # cleanup = "uninstall";  # Homebrew からの移行完了後に設定
+    };
+
     casks = [
       "google-chrome"
     ];
