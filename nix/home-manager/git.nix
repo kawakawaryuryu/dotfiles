ae48f05391b84fa4321b521f7ec9ac1c893730b2
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-03-26 23:01:08 +0900

    fix: git.nix

--- nix/home-manager/git.nix
+++ nix/home-manager/git.nix
@@ -1,28 +1,30 @@
-programs.git = {
-  enable = true;
-  settings = {
-    user = {
-      name = "Ryutaro Kawaguchi";
-      email = "kawakawaryuryu@hotmail.co.jp";
+{
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Ryutaro Kawaguchi";
+        email = "kawakawaryuryu@hotmail.co.jp";
+      };
+      alias = {
+        st = "status";
+        df = "diff";
+        co = "checkout";
+        b = "branch";
+        ad = "add";
+        cm = "commit";
+        p = "push";
+      };
+      init = {
+        defaultBranch = "main";
+      };
     };
-    alias = {
-      st = status;
-      df = diff;
-      co = checkout;
-      b = branch;
-      ad = add;
-      cm = commit;
-      p = push;
-    };
-    init = {
-      defaultBranch = main;
-    };
+    ignores = [
+      "*.swp"
+      ".DS_Store"
+      "GPATH"
+      "GRTAGS"
+      "GTAGS"
+    ];
   };
-  ignores = [
-    "*.swp"
-    ".DS_Store"
-    "GPATH"
-    "GRTAGS"
-    "GTAGS"
-  ];
-};
+}
