167a314ae26d11c176a917e5fb967ea17b27590b
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-04-09 23:51:25 +0900

    chore: add comment

--- nix/home-manager/ghostty.nix
+++ nix/home-manager/ghostty.nix
@@ -4,5 +4,9 @@
     enable = true;
     package = pkgs.ghostty-bin;
     enableFishIntegration = true;
+    # chsh -s /run/current-system/sw/bin/fishによって有効化しているためコメントアウト
+    #settings = {
+    #  command = "/etc/profiles/per-user/ryu/bin/fish";
+    #};
   };
 }
