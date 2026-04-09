50b269f2d8d153d4cd343426d1add3e8c402e5e0
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-04-10 00:52:40 +0900

    feat: enable fish theme

--- nix/home-manager/fish.nix
+++ nix/home-manager/fish.nix
@@ -9,8 +9,8 @@
 	src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
 	  repo = "theme-bobthefish";
-	  rev = "";
-	  sha256 = "c5efbe05aed81b201454c0ae1190ba91ea1970ac";
+	  rev = "c5efbe05aed81b201454c0ae1190ba91ea1970ac";
+	  sha256 = "sha256-12Xd43vy6qQILV/Q5BeoGaul6DsQv5OloCPLXwR6KNU=";
 	};
       }
     ];
