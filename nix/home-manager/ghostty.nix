2a7e5a242005022975c3f93ba6cb8dfe25d2d2fb
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-04-10 23:57:38 +0900

    feat: restore ghostty without quick terminal

--- nix/home-manager/ghostty.nix
+++ nix/home-manager/ghostty.nix
@@ -10,12 +10,6 @@
 
       # 背景の透明度
       background-opacity = 0.9;
-
-      # hotkeyでターミナルをいつでも出す
-      keybind = "global:cmd+backquote=toggle_quick_terminal";
-      quick-terminal-size = "100%";
-      quick-terminal-animation-duration = 0;
-      quick-terminal-autohide = false;
     };
   };
 }
