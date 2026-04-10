c0ec8fdc3288a348a8f45977e6d4810a3faae2d3
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-04-10 22:17:16 +0900

    feat: set background-opacity of ghostty

--- nix/home-manager/ghostty.nix
+++ nix/home-manager/ghostty.nix
@@ -4,9 +4,12 @@
     enable = true;
     package = pkgs.ghostty-bin;
     enableFishIntegration = true;
-    # chsh -s /run/current-system/sw/bin/fishによって有効化しているためコメントアウト
-    #settings = {
-    #  command = "/etc/profiles/per-user/ryu/bin/fish";
-    #};
+    settings = {
+      # chsh -s /run/current-system/sw/bin/fishによって有効化しているためコメントアウト
+      #command = "/etc/profiles/per-user/ryu/bin/fish";
+
+      # 背景の透明度
+      background-opacity = 0.9;
+    };
   };
 }
