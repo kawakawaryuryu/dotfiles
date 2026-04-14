3719e18b107639220753a8d1a755de64bf392d34
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-04-15 00:21:39 +0900

    feat: specify font size and set use_ime true

--- nix/home-manager/wezterm/wezterm.lua
+++ nix/home-manager/wezterm/wezterm.lua
@@ -2,11 +2,14 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+-- 設定変更を自動で読み込み
+config.automatically_reload_config = true
+
 -- 背景の透明度
 config.window_background_opacity = 0.9
 
--- changing the font size and color scheme.
---config.font_size = 10
---config.color_scheme = 'AdventureTime'
+config.font_size = 12.5
+
+config.use_ime = true
 
 return config
