7d7a68fc86628d68806a61c02869d43945263eef
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2026-05-14 00:32:19 +0900

    refactor: set nonumberrelativ of neovim

--- nix/home-manager/neovim/config/lua/config/options.lua
+++ nix/home-manager/neovim/config/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 行番号の表示
+vim.opt.number = true
+-- 行番号を相対位置ではなく絶対位置として表示
+vim.opt.relativenumber = false
